98a7e5019c780dfcad6a033617dde520a88901b7
Krishnakant Kushwaha <145730098+KRISHNAKANT-KUSHWAHA@users.noreply.github.com>
2026-07-02 11:14:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -29,6 +29,6 @@ function insert_Row() {
     row.appendChild(cell1);
     row.appendChild(cell2);
 
-    // Insert the new row at the top of the table
-    table.insertBefore(row, table.firstChild);
+    // Insert the new row at the top of the table 
+    table.insertBefore(row, table.rows[0]);
 }
